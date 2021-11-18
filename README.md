948800621541da609c8d0946840aa097607a680b
RCP-2001 <coalgolem.xd@gmail.com>
2021-11-18 14:00:24 -0800

    Init Stuff

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Football-Golf-Bot
- 
+# FootballGolfBot
+    Final design plan: make a fantasy football bot, but the winner the homie with tha lowest score
+
+    Intended for managment by me, but if you want to use it, youll need to create an auth token
+
+    Tutorial: https://www.digitaltrends.com/gaming/how-to-make-a-discord-bot/ 
+    (not me, im just a theif lmao)
+
+    if you have any problems: https://www.youtube.com/watch?v=F0uvt97Xn20 
---
+++ main.js
@@ -0,0 +1,54 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+
+// configure logger settings
+//this might be more for the tutorial
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+
+logger.level = 'debug';
+
+// Initialize Discord Bot
+var bot = new Discord.Client({
+    token: auth.token,
+    autorun: true
+});
+
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('0Logged in as: ')
+    logger.info(bot.username + ' - (' + bot.id + ')');
+
+});
+
+bot.on('message', function (user, userID, channelID, message, evt) {
+    //listen for messages that start with !
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+
+        // the real shit
+        switch (cmd) {
+            //ping (testing)
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: "suck my ass"
+                });
+                break;
+            
+            case 'cum':
+                bot.sendMessage({
+                    to: channelID,
+                    message: "Harder Daddy"
+                });
+                break;
+            
+        }
+    }
+})
+
+// https://www.digitaltrends.com/gaming/how-to-make-a-discord-bot/
